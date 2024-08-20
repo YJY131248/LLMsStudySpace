aec0fcbe3066d7819cf5dd404ef222d1f699580e
Jinyu Yao <62458821+YJY131248@users.noreply.github.com>
2024-08-20 18:46:34 +0800

    create utils.py

---
+++ LlamaIndexStudy/src/utils.py
@@ -0,0 +1,20 @@
+def messages_to_prompt(messages):
+    prompt = ""
+    for message in messages:
+        if message.role == "system":
+            prompt += f"<|im_start|>system\n{message.content}<|im_end|>\n"
+        elif message.role == "user":
+            prompt += f"<|im_start|>user\n{message.content}<|im_end|>\n"
+        elif message.role == "assistant":
+            prompt += f"<|im_start|>assistant\n{message.content}<|im_end|>\n"
+
+    if not prompt.startswith("<|im_start|>system"):
+        prompt = "<|im_start|>system\n" + prompt
+
+    prompt = prompt + "<|im_start|>assistant\n"
+
+    return prompt
+
+
+def completion_to_prompt(completion):
+   return f"<|im_start|>system\n<|im_end|>\n<|im_start|>user\n{completion}<|im_end|>\n<|im_start|>assistant\n"
