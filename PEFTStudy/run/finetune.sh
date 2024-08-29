d95dd1670978b58642325e0cf8e00bf1a6aa45c1
Jinyu Yao <62458821+YJY131248@users.noreply.github.com>
2024-08-29 17:57:30 +0800

    update finetune.sh

--- PEFTStudy/run/finetune.sh
+++ PEFTStudy/run/finetune.sh
@@ -5,7 +5,7 @@ mlx worker launch -- python3 ../src/finetune.py \
     --dataset_path ../data/alpaca_gpt4_data_zh.json \
     --log_path ../out/lora_output.log \
     --max_length 256 \
-    --lora_rank 8 \
+    --lora_rank 4 \
     --output_dir ../out/lora_peft \
     --per_device_train_batch_size 1 \   
     --per_device_eval_batch_size 1 \
