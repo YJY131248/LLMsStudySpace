582aeb10f7e62fd0607761e3e2c9e4dfdc844eba
Jinyu Yao <62458821+YJY131248@users.noreply.github.com>
2024-08-29 19:57:10 +0800

    update merge_model.sh

--- PEFTStudy/run/merge_model.sh
+++ PEFTStudy/run/merge_model.sh
@@ -2,7 +2,7 @@ mlx worker launch -- python3 ../src/merge_model.py \
     --peft_type lora \
     --llm_model_name Qwen \
     --llm_model_path ../../../model/Qwen2-7B-Instruct \
-    --peft_checkpoint_path ../out/lora_peft/checkpoint-500/ \
+    --peft_checkpoint_path ../out/lora_peft/checkpoint-3000/ \
     --merge_save_path ../out/lora_peft/lora_model_end/ \
-    --log_path ../out/lora_output.log \
+    --log_path ../log/lora_output.log \
     
