4b75ca5cd3860df73ed0705c8ec09fc726bb94fc
Jinyu Yao <62458821+YJY131248@users.noreply.github.com>
2024-08-29 22:01:39 +0800

    update inference.sh

--- PEFTStudy/run/inference.sh
+++ PEFTStudy/run/inference.sh
@@ -1,7 +1,7 @@
 mlx worker launch -- python3 ../src/inference.py \
     --llm_model_name Qwen \
     --llm_model_path ../../../model/Qwen2-7B-Instruct \
-    --peft_type p-tuning \
-    --merge_save_path ../out/p-tuning_peft/p-tuning_model_end/ \
+    --peft_type lora \
+    --merge_save_path ../out/p-tuning_peft/lora_model_end/ \
     --use_merge_model True \
-    --log_path ../out/p-tuning_output.log \
+    --log_path ../out/lora_output.log \
