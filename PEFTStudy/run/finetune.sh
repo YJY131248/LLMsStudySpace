ddff4f0b02874eb284a232ff25eac1a93116dc6f
Jinyu Yao <62458821+YJY131248@users.noreply.github.com>
2024-08-29 19:58:31 +0800

    update finetune.sh

--- PEFTStudy/run/finetune.sh
+++ PEFTStudy/run/finetune.sh
@@ -3,10 +3,10 @@ mlx worker launch -- python3 ../src/finetune.py \
     --llm_model_name Qwen \
     --llm_model_path ../../../model/Qwen2-7B-Instruct \
     --dataset_path ../data/alpaca_gpt4_data_zh.json \
-    --log_path ../out/lora_output.log \
+    --log_path ../log/lora_output.log \
     --max_length 256 \
     --lora_rank 4 \
-    --output_dir ../log/lora_peft \
+    --output_dir ../out/lora_peft \
     --per_device_train_batch_size 1 \   
     --per_device_eval_batch_size 1 \
     --num_train_epochs 1 \
